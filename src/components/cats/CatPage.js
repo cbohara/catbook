7eb5127f62ddcbe78e641efd89910057df1a2712
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-24 20:09:25 -0700

    completed show features

--- src/components/cats/CatPage.js
+++ src/components/cats/CatPage.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as catActions from '../../actions/catActions';
 import CatList from './CatList';
+import HobbyList from './HobbyList';
 
 class CatPage extends React.Component {
   render() {
@@ -11,13 +12,15 @@ class CatPage extends React.Component {
         <p>breed: {this.props.cat.breed}</p>
         <p>weight: {this.props.cat.weight}</p>
         <p>temperament: {this.props.cat.temperament}</p>
+        <HobbyList hobbies={this.props.catHobbies} />
       </div>
     );
   }
 };
 
 CatPage.propTypes = {
-  cats: PropTypes.array.isRequired,
+  cat: PropTypes.object.isRequired,
+  catHobbies: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -28,5 +31,27 @@ function mapStateToProps(state, ownProps) {
   }
   return {cat: cat};
 }
+
+function collectCatHobbies(hobbies, cat) {
+  let selected = hobbies.map(hobby => {
+    if (cat.hobby_ids.filter(hobbyId => hobbyId  hobby.id).length > 0) {
+      return hobby;
+    }
+  })
+  return selected.filter(el => el != undefined)
+}
+
+function mapStateToProps(state, ownProps) {
+  let cat = {name: '', breed: '', weight: '', temperament: '', hobby_ids: []};
+  let Cathobbies = []
+  const catId = ownProps.params.id;
+  if (state.cats.length > 0 && state.hobbies > 0) {
+    cat = Object.assign({}, state.cats.find(cat => cat.id == id)
+    if (cat.hobby_ids.length > 0) {
+      hobbies = collectCatHobbies(state.hobbies, cat)
+    }
+  }
+  return {cat: cat, catHobbies: catHobbies};
+};
 
 export default connect(mapStateToProps)(CatPage);
